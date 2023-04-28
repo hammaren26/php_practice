f6af9407a4bba78129931f17d3ae6b997f28cac5
Vyacheslav <hammaren23@gmail.com>
2023-04-28 18:51:59 +0600

    new commit

--- index.php
+++ index.php
@@ -2,11 +2,35 @@
 
 error_reporting(-1);
 
+$arrTest = [
+    'one' => [
+        '1',
+        '2'
+    ]
+];
 
-define('MAIN', 123);
+$arr2 = [
+    'one' => 'sdf',
+    'two' => 'gfdgf',
+    'three' => [
+        1, 2, 3
+    ]
+];
 
-const MAIN2 = 456;
+extract($arr2);
+
+//PHP Цикл for в PHP - 20:28
+
+?>
+
+<pre>
+   <? echo $one; ?>
+</pre>
+
+<script>
+    console.log({
+        'RESULT': <?= json_encode() ?>,
+    })
+</script>
 
 
-echo MAIN;
-echo MAIN2;
