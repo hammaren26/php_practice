c059d9ca2d50eabd0ea0358f20c1d4ba303bcc0a
Vyacheslav <hammaren23@gmail.com>
2023-05-18 18:37:19 +0600

    header() php lesson

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 error_reporting(-1);
 
+//header("Location: https://mahog55.ru/");
+//header('refresh: 5; url=index.php');
+//header('HTTP/1.1 200 OK');
+//header('HTTP/1.1 304 Not Modified');
+header('Content-Type: text/plain; charset=utf-8');
+header('Content-Disposition: attachment; filename="test.txt"');
+
+
+//die();
+
+
 $arrTest = [
     'one' => [
         '1',
@@ -19,12 +30,26 @@ $arr2 = [
 
 extract($arr2);
 
-//PHP Цикл for в PHP - 20:28
-
 ?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+
 
 <pre>
    <? echo $one; ?>
+
+    hello!!!!!
 </pre>
 
 <script>
@@ -32,5 +57,11 @@ extract($arr2);
         'RESULT': <?= json_encode() ?>,
     })
 </script>
+
+
+</body>
+</html>
+
+
 
 
