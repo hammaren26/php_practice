c1d7f33c213d1018a4e865ea8c1b397eb99c48d1
Vyacheslav <hammaren23@gmail.com>
2023-05-25 13:10:50 +0600

    date functions

--- index.php
+++ index.php
@@ -1,67 +1,34 @@
 <?
-
-error_reporting(-1);
-
-//header("Location: https://mahog55.ru/");
-//header('refresh: 5; url=index.php');
-//header('HTTP/1.1 200 OK');
-//header('HTTP/1.1 304 Not Modified');
-header('Content-Type: text/plain; charset=utf-8');
-header('Content-Disposition: attachment; filename="test.txt"');
-
-
-//die();
-
-
-$arrTest = [
-    'one' => [
-        '1',
-        '2'
-    ]
-];
-
-$arr2 = [
-    'one' => 'sdf',
-    'two' => 'gfdgf',
-    'three' => [
-        1, 2, 3
-    ]
-];
-
-extract($arr2);
-
+function debug($data)
+{
+    echo '<pre>' . print_r($data, 1) . '</pre>';
+}
 ?>
 
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<? date_default_timezone_set('Asia/Omsk');
+//echo date('d:m:Y H:i:s', time() + (60 * 60 * 24 * 10));
+//$date = getdate();
+//debug($date);
+//debug(
+//    date('d:m:Y H:i:s', strtotime("+ 2 day"))
+//);
 
 
+$date = date_create(date('Y-m-d H:i:s'));
+
+debug($date);
+
+echo date_format($date, 'Y-m-d H:i:s');
+
+date_add($date, date_interval_create_from_date_string('2 days + 2 hours + 10 minutes + 5 seconds'));
+
+echo '<hr>';
+echo date_format($date, 'Y-m-d H:i:s');
+
+echo '<hr>';
+
+usleep(1000000 * 5);
 
 
-<pre>
-   <? echo $one; ?>
-
-    hello!!!!!
-</pre>
-
-<script>
-    console.log({
-        'RESULT': <?= json_encode() ?>,
-    })
-</script>
-
-
-</body>
-</html>
-
-
-
-
+echo microtime();
